4e51d47b28479e2b618398f7a35aab566c08db7e
Harovinkin <kharovinkin@gmail.com>
2020-12-28 16:30:17 +0200

    created hoc

--- src/index.js
+++ src/index.js
@@ -1,14 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const starWarsChars = [
+  {name: 'Дарт Вейдер', side: 'dark'},
+  {name: 'Палпатин', side: 'dark'},
+  {name: 'Люк Скайуокер', side: 'light'},
+  {name: 'Обиван Кеноби', side: 'light'},
+]
+
+const App = ({list}) => {
+  return (
+    <ul>
+      {
+        list.map((item, i) => (
+          <li
+            key={item.name + i}
+          >
+            <strong>
+              {item.name}
+            </strong>
+            {`- ${item.side}`}
+          </li>
+        ))
+      }
+    </ul>
+  )
+}
+
+const withFilteredChars = Component => ({list, side}) => {
+  const filteredList = list.filter(item => item.side === side)
+  return <Component list={filteredList} />
+}
+
+const FilteredList = withFilteredChars(App)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FilteredList
+      list={starWarsChars}
+      side="dark"
+    />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') 
 );
 
 // If you want to start measuring performance in your app, pass a function
